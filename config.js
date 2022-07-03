b36ffa68be88f0f57d7ff649bdd0606b6244efdf
ogulcanbilece <ogulcanbilece@gmail.com>
2022-07-03 16:40:42 +0200

    First Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3130267340635863',
+        clientSecret: '8a84b6670a0cc354291801352659ce22'
+    }
+}
